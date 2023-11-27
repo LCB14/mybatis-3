0ad8324e4eec481ef94cee90704c9c6299067c20
changbaol <1642272412@qq.com>
2023-11-27 18:48:40 +0800

    2023年11月27日18:48:38

---
+++ src/main/java/com/lcb/jdbc/TestJDBC.java
@@ -0,0 +1,71 @@
+package com.lcb.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author changbao.li Date: 2019-10-01 Time: 17:39
+ * @version $
+ */
+public class TestJDBC {
+
+    /**
+     * <P>Applications no longer need to explicitly load JDBC drivers using <code>Class.forName()</code>. Existing programs
+     *  * which currently load JDBC drivers using <code>Class.forName()</code> will continue to work without
+     *  * modification.
+     *
+     * @see DriverManager
+     *
+     * 无须手动加载驱动了！！
+     */
+    static {
+        try {
+            // 1、加载驱动
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            // 2、创建连接
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://127.0.0.1:3306/test_db?useSSL=false&characterEncoding=utf-8", "root",
+                            "li123456");
+            // 3、预编译SQL
+            preparedStatement = connection.prepareStatement("select * from student where id = ?");
+            preparedStatement.setInt(1, 1);
+
+            // 4、执行SQL
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                String columnName1 = resultSet.getMetaData().getColumnName(1);
+                String columnName2 = resultSet.getMetaData().getColumnName(2);
+                System.out.println(columnName1 + ":" + resultSet.getInt(1));
+                System.out.println(columnName2 + ":" + resultSet.getString(2));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/lcb/mybatis/StudentMapperTest.java
@@ -0,0 +1,66 @@
+package com.lcb.mybatis;
+
+import com.lcb.beans.Student;
+import com.lcb.mapper.StudentMapper;
+import org.apache.ibatis.binding.MapperProxy;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+public class StudentMapperTest {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        Reader reader = null;
+        try {
+            // 通过MyBatis自带的Resources工具来加载配置文件
+            reader = Resources.getResourceAsReader("mybatis/mybatis-config.xml");
+            // 通过SqlSessionFactoryBuilder()来构建sqlSessionFactory对象
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        // 打开一个session对象
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = sqlSessionFactory.openSession();
+            /**
+             * 通过session对象来获取mapper
+             *
+             * @see MapperProxy#invoke(Object, java.lang.reflect.Method, Object[])
+             */
+            StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+            List<Student> studentList = studentMapper.selectAll();
+            System.out.println(studentList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 记得关闭session
+            if (sqlSession != null) {
+                try {
+                    sqlSession.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
